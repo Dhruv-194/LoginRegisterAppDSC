e19d529614d9b3ff97e969086b51c9b04ac40cd0
Dhruv Mehta <61137052+Dhruv-194@users.noreply.github.com>
2021-07-29 23:30:08 +0530

    added mainactivity

---
+++ Android Studio Projects/app/src/main/java/com/example/loginregisterappdsc/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.loginregisterappdsc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button:Button = findViewById(R.id.LoginBtn)
+        button.setOnClickListener{
+            val intent = Intent(this@MainActivity,
+                RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
